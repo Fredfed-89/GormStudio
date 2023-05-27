1ff972fec49f149b95ca02c8c75f343c2f9837a6
Fredfed-89 <fedyaev.sergei89@gmail.com>
2023-05-27 23:53:44 +0300

    start responsive

--- src/js/components/hamburger.js
+++ src/js/components/hamburger.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".header__nav-list");
+const navMenu = document.querySelector(".header__nav");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
-  navMenu.classList.toggle("is-active");
+  navMenu.classList.toggle("active");
 });
