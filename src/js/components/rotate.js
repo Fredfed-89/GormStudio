65edf2b076d5bfd13ab29be542734c48dcaceed5
Fredfed-89 <fedyaev.sergei89@gmail.com>
2023-05-28 19:41:35 +0300

    98 percent of desktop

--- src/js/components/rotate.js
+++ src/js/components/rotate.js
@@ -1,4 +1,4 @@
-const text = document.querySelector('.text');
+const text = document.querySelector('.circle__text-btn');
     text.innerHTML = text.innerText.split("").map(
       (char, i) =>
       `<span style="transform: rotate(${i * 23}deg)">${char}</span>`
